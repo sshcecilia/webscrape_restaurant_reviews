4797641bda0fcb55e93fc6a2696f6bad533625d5
sshcecilia <sshcecilia@hotmail.com>
2024-09-08 12:05:50 +0800

    Remove redundant imports

--- webscraping/webscrape_reviews.py
+++ webscraping/webscrape_reviews.py
@@ -1,14 +1,9 @@
-import webscrape_restaurants
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
-import re
 import time
-import pandas as pd
 
 def extract_info(driver):
     
