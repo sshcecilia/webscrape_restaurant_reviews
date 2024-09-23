b32a0de64c5e95d91152c9a4127f36f7cafc953e
sshcecilia <sshcecilia@hotmail.com>
2024-09-23 17:02:03 +0800

    Clean up code into a class object

--- webscraping/webscrape_restaurants.py
+++ webscraping/webscrape_restaurants.py
@@ -1,271 +1,121 @@
-from selenium import webdriver
+import time
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-import time
+from selenium.common.exceptions import TimeoutException
+from datetime import date
+import pandas as pd
+import file_setup
 
-def access_webpage(url):
+class restaurants():
     """
-    Open and return the driver to the url provided
-
-    :param url: string object containing url of a website 
-    """
-    driver = webdriver.Chrome()
-    driver.set_page_load_timeout(300)
-    driver.get(url)
-    time.sleep(3)
-    try:
-        driver.maximize_window()
-    except:
-        pass
-    
-    return driver
-
-def search_scroll(driver, query, max_scroll = -1):
-    """
-    Search based on the query provided and scroll down the website to get the full list of restaurants
-
-    :param driver: webdriver object
-    :param query: keyword to enter into the searchbox
-    :param max_scroll: int containing the number of times to scroll. default is -1 to scroll until the end of the page.
-    """
-    wait = WebDriverWait(driver, 30)
-    searchbox = wait.until(EC.visibility_of_element_located((By.ID, 'searchboxinput')))
-
-    searchbox.clear()
-    searchbox.send_keys(query)
-    searchbox.send_keys(Keys.RETURN)
-
-    divSideBar = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,f"div[aria-label='Results for " + query + "']")))
-
-    keepScrolling = True
-    while(keepScrolling):
-        driver.execute_script('arguments[0].scrollTo(0, arguments[0].scrollHeight)', divSideBar)
-        time.sleep(1.2)
-        html = wait.until(EC.visibility_of_element_located((By.TAG_NAME, "html"))).get_attribute('outerHTML')
-        if ((html.find("You've reached the end of the list.") != -1) | max_scroll == 0):
-            keepScrolling = False
-        max_scroll -= 1
-
-def restaurant_list(driver):
-    """
-    Obtain a list of elements for the restaurants extracted from the webpage
-    
-    :param driver: webdriver object
-    """
-    raw = driver.find_elements(By.CSS_SELECTOR, "div.Nv2PK")
-    return raw
-
-def get_basic(element):
-    """
-    Extract the following information of a restaurant
-    - Name of the restaurants
-    - Google maps link of the restaurant
-    - Status of the restaurant (whether the store is operating)
-    - Ratings and Number of reviews
-
-    :param element: An element of a webpage
-    """
-    name = element.find_elements(By.CSS_SELECTOR, "a.hfpxzc")[0].get_attribute('aria-label')
-    href = element.find_elements(By.CSS_SELECTOR, "a.hfpxzc")[0].get_attribute('href')
-    info = element.find_elements(By.CSS_SELECTOR, "span.ZkP5Je")[0].get_attribute('aria-label')
-    
-    # If Status is not available, set it to 'Open'. If number of reviews are not available
-    if (len(element.find_elements(By.CSS_SELECTOR, "span.eXlrNe")) == 0):
-        status = 'Open'
-    else:
-        status = element.find_elements(By.CSS_SELECTOR, "span.eXlrNe")[0].text
-    return name, href, status, info
-
-def get_details(driver):
-    """
-    Extract additional information of a restaurant from its Google Maps page
-    - Type of Restaurant (e.g. Hawker Center, Bar)
-    - Price level and label to get an indication of how expensive the restaurant is
-    - Address
-    - Restaurant's wensite if available
-
-    param driver: webdriver object
-    """
-    time.sleep(2)
-    restaurant_type = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.skqShb"))).text.split('\n')[-1]
-    address = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.rogA2c "))).text
-    price_label = ''
-    price_level = ''
-    website = ''
-
-    try:
-        price_label = driver.find_element(By.XPATH, "//span[@class = 'mgr77e']/span/span/span/span").get_attribute('aria-label')
-        price_level = driver.find_element(By.XPATH, "//span[@class = 'mgr77e']/span/span/span/span").text
-    except (TimeoutException, NoSuchElementException):
-        pass
-
-    try:
-        website = driver.find_element(By.CSS_SELECTOR, "a.CsEnBe").get_attribute('href')
-    except (TimeoutException, NoSuchElementException):
-        pass
-    
-    return restaurant_type, price_label, price_level, address, website
-
-def get_latlong(driver):
-    """
-    Get latitude and longitude of restaurant
-
-    param driver: webdriver object
-    """
-    str_error = ''
-    try:
-        WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.XPATH,"//html/body")))
-        element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH,"//html/body")))
-        ActionChains(driver).move_to_element(element).context_click().perform()
-    except TimeoutException as str_error:
-        pass   
-    if str_error:
-        time.sleep(2)
-        WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.mLuXec")))
-        element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.mLuXec")))
-
-        lat_long = element.text
-    else:
-        pass
-    
-    str_error = ''
-    try:
-        WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.mLuXec")))
-        element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.mLuXec")))
-        lat_long = element.text
-    except TimeoutException as str_error:
-        pass 
-    if str_error:
-        time.sleep(2)
-        WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.mLuXec")))
-        element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.mLuXec")))
-
-        lat_long = element.text
-    else:
-        pass
-
-    return lat_long
-
-def get_ophours(driver):
-    """
-    Get operating hours of the restaurant
-
-    param driver: webdriver object
-    """
-    time.sleep(5)
-    op_hours = ''
-    to_error = ''
-    ne_error = ''
-    
-    try:
-        WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.XPATH,"//span[@aria-label = 'Show open hours for the week']")))
-        element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH,"//span[@aria-label = 'Show open hours for the week']")))
-        element.click()
-        time.sleep(3)
-
-        op_hours = WebDriverWait(driver, 30).until(EC.visibility_of_all_elements_located((By.XPATH, "//table[@class = 'eK4R0e fontBodyMedium']")))[0].text
-
-    except TimeoutException as to_error:
-        pass
-    
-    except NoSuchElementException as ne_error:
-        pass
-
-    if to_error:
-        WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.XPATH,"//span[@aria-label = 'Show open hours for the week']")))
-        element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH,"//span[@aria-label = 'Show open hours for the week']")))
-        element.click()
-        time.sleep(3)
-
-    elif ne_error:
-        try:
-            WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.XPATH,"//button[contains(@aria-label, 'hours')]")))
-            element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH,"//button[contains(@aria-label, 'hours')]")))
-            element.click()
-            time.sleep(3)
-
-            for i in driver.find_elements(By.XPATH, "//div[@aria-expanded='false']"):
-                i.click()
-            op_hours = WebDriverWait(driver, 30).until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@class = 'm6QErb DxyBCb kA9KIf dS8AEf XiKgde ']")))[0].text
-            element = driver.find_element(By.XPATH, "//button[@aria-label= 'Back']")
-            element.click()
-
-        except TimeoutException as to_error:
-            pass
+    Obtain a list of restaurants near specific region from Google Maps
         
-        except NoSuchElementException:
-            pass
-
-        if to_error:
-            WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.XPATH,"//button[contains(@aria-label, 'hours')]")))
-            element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH,"//button[contains(@aria-label, 'hours')]")))
-            element.click()
-            time.sleep(3)
-
-            for i in driver.find_elements(By.XPATH, "//div[@aria-expanded='false']"):
-                i.click()
-            op_hours = WebDriverWait(driver, 30).until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@class = 'm6QErb DxyBCb kA9KIf dS8AEf XiKgde ']")))[0].text
-            element = driver.find_element(By.XPATH, "//button[@aria-label= 'Back']")
-            element.click()
-
-    return op_hours
-
-def get_poptime(driver):
+    :param region (str): region in which the nearby restaurants will be extracted
+    :param max_scroll (int): the number of times to scroll
+                             default value is -1 which will scroll till the end of the page
+    :param max_tries (int): the number of times to retry scrolling the webpage should the webpage takes too long to load
+                            default value is 5
     """
-    Get popular times based on Google Maps
+    def __init__(self, region, max_scroll = -1, max_tries = 5):
+        self.driver = file_setup.access_webpage(url = "https://www.google.com/maps")
+        self.region = region
+        self.max_scroll = max_scroll
+        self.max_tries = max_tries
 
-    param driver: webdriver object
-    """
+    def restaurant_list(self):
+        """
+        Obtain a list of elements for the restaurants extracted from the webpage
+        """
+        raw = self.driver.find_elements(By.CSS_SELECTOR, "div.Nv2PK")
+        return raw
 
-    poptime = ''
-    time.sleep(2)
+    def search_scroll(self, query):
+        """
+        Search based on the query provided and
+            scroll down the website to get the full list of restaurants
 
-    try:
-        for i in range(7):
-            element = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.XPATH, "//div[@class = 'goog-inline-block goog-menu-button-dropdown']")))
-            element.click()
-            element = WebDriverWait(driver, 30).until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@class = 'goog-menuitem-content']")))[i]
-            busy = element.text + ": "
-            element.click()
-            time.sleep(0.5)
-            for j in WebDriverWait(driver, 30).until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@class = 'g2BVhd eoFzo ']/div"))):
-                try:
-                    busy += j.get_attribute('aria-label').replace("\u202f", "") + '; '
-                except:
-                    busy += j.text + '; '     
+        :param query (str): keyword to enter into the searchbox
+        """
+        wait = WebDriverWait(self.driver, 30)
+        searchbox = wait.until(
+                        EC.visibility_of_element_located(
+                            (By.ID, 'searchboxinput')))
 
-            poptime += busy
+        searchbox.clear()
+        searchbox.send_keys(query)
+        searchbox.send_keys(Keys.RETURN)
 
-    except (TimeoutException, NoSuchElementException):
-        pass
+        sidebar = wait.until(
+                    EC.visibility_of_element_located(
+                        (By.CSS_SELECTOR, "div[aria-label='Results for " + query + "']")))
 
-    return poptime
+        keepscrolling = True
+        num_restaurants = 0
+        prev_num_restaurants = 0
+        counter = 0
+        while keepscrolling:
+            self.driver.execute_script('arguments[0].scrollTo(0, arguments[0].scrollHeight)',
+                                sidebar)
+            time.sleep(1.2)
+            html = wait.until(
+                    EC.visibility_of_element_located(
+                        (By.TAG_NAME, "html"))).get_attribute('outerHTML')
+            prev_num_restaurants = num_restaurants
+            num_restaurants = len(self.restaurant_list())  
+            if (html.find("You've reached the end of the list.") != -1) | self.max_scroll == 0:
+                keepscrolling = False
+            elif counter >= self.max_tries:
+                keepscrolling = False
+            elif num_restaurants == prev_num_restaurants:
+                counter += 1
+            self.max_scroll -= 1
 
-def get_service(driver):
-    """
-    Get services available at a restaurant
+    def get_basic(self, element):
+        """
+        Extract the following information of a restaurant
+        - Name of the restaurants
+        - Google maps link of the restaurant
+        - Status of the restaurant (whether the store is operating)
+        - Ratings and Number of reviews
 
-    param driver: webdriver object
-    """
+        :param element: An element of a webpage
+        """
+        try:
+            name = element.find_elements(By.CSS_SELECTOR,"a.hfpxzc")[0].get_attribute('aria-label')
+            href = element.find_elements(By.CSS_SELECTOR, "a.hfpxzc")[0].get_attribute('href')
+            info = element.find_elements(By.CSS_SELECTOR, "span.ZkP5Je")[0].get_attribute('aria-label')
 
-    time.sleep(5)
-    service = ''
+            # If Status is not available, set it to 'Open'. If number of reviews are not available
+            if len(element.find_elements(By.CSS_SELECTOR, "span.eXlrNe")) == 0:
+                status = 'Open'
+            else:
+                status = element.find_elements(By.CSS_SELECTOR, "span.eXlrNe")[0].text
+        except:
+            name = ''
+            href = ''
+            status = ''
+            info = ''
+    
+        return name, href, status, info
 
-    ## Service Options
-    try:
-        element = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.XPATH, "//div[contains(text(), 'About')]")))
-        element.click()
-        time.sleep(2)
+    ## Extraction of List of Restaurants
 
-        service_list = WebDriverWait(driver, 30).until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@class='m6QErb DxyBCb kA9KIf dS8AEf XiKgde ']")))
-        for i in service_list:
-            service += i.text
-        element = driver.find_element(By.XPATH, "//button[@aria-label= 'Back']")
-        element.click()
-    except (TimeoutException, NoSuchElementException):
-        pass
-    return service
+    def get_restaurant(self):
+        """
+        Compile the extracted information into a Pandas DataFrame and update when the information was last updated
+        Close the driver once the task is completed
+        """
+        try:
+            self.search_scroll(f"Food in {self.region}")
+            restaurants_element = self.restaurant_list()
+            restaurants_subset = pd.DataFrame(map(self.get_basic, restaurants_element), columns = ['restaurant_name', 'href', 'status', 'info'])
+            restaurants_subset.drop(restaurants_subset[restaurants_subset['href'] == ''].index, inplace=True)
+            restaurants_subset[['details_last_updated']] = date(1900,1,1)
+            restaurants_subset[['reviews_last_updated']] = date(1900,1,1)
+        except TimeoutException:
+            restaurants_subset = None
+
+        self.driver.close()
+        return restaurants_subset
